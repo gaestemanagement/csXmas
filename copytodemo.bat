02a2ae3574087f4122b11e0b7c64e866aee7dc61
mkogler-novacom <m.kogler@novacom.at>
2015-11-22 15:56:08 +0100

    HTML

---
+++ copytodemo.bat
@@ -0,0 +1,4 @@
+xcopy build\* \\192.168.0.10\sw-auslieferung\auto-update\demo\csXmas\build\ /Y /S /E
+xcopy res\* \\192.168.0.10\sw-auslieferung\auto-update\demo\csXmas\res\ /Y /S /E
+xcopy src\* \\192.168.0.10\sw-auslieferung\auto-update\demo\csXmas\src\ /Y /S /E
+xcopy index* \\192.168.0.10\sw-auslieferung\auto-update\demo\csXmas\ /Y
